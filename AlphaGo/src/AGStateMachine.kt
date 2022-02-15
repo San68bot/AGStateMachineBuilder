76744347ad60da55c54d354c71beea42b59eedf6
sanjay <san68bot@gmail.com>
2022-02-14 19:51:15 -0500

    state switching

--- AlphaGo/src/AGStateMachine.kt
+++ AlphaGo/src/AGStateMachine.kt
@@ -1,10 +1,19 @@
 class AGStateMachine(private val mainBlock: AGStateMachine.() -> Unit) {
     val states = mutableListOf<AGState>()
-    var currentState = 0
+    private var currentState = 0
+    val runningState
+        get() = states[currentState]
+
+    private val oneTimes = arrayListOf(OneTime(), OneTime())
 
     init {
+        oneTimes.forEach { it.reset() }
         states.clear()
         mainBlock()
+    }
+
+    private fun resetTransition() {
+        oneTimes.forEach { it.reset() }
     }
 
     fun state(name: String, block: AGState.() -> Unit) {
@@ -17,7 +26,13 @@ class AGStateMachine(private val mainBlock: AGStateMachine.() -> Unit) {
     }
 
     fun run() {
-        states[currentState].enterAction?.invoke()
+        states.forEach {
+            if (it == states[currentState]) {
+                oneTimes.first().runAction { states[currentState].enterAction?.invoke() }
+                //loop
+                oneTimes.last().runAction { states[currentState].exitAction?.invoke() }
+            }
+        }
     }
 
     fun AGState.enter(block: () -> Unit) {
@@ -30,6 +45,12 @@ class AGStateMachine(private val mainBlock: AGStateMachine.() -> Unit) {
 
     fun AGState.exit(block: () -> Unit) {
         states.last().exitAction = block
+    }
+
+    fun nextState(name: String) {
+        resetTransition()
+        println("eek: ${states.indexOf(states.find { it.name == name })}")
+        currentState = states.indexOf(states.find { it.name == name })
     }
 }
 
--- AlphaGo/src/test.kt
+++ AlphaGo/src/test.kt
@@ -4,19 +4,21 @@ fun main() {
     val agStateMachine = AGStateMachine {
         state("s1") {
             enter {
-                this.name = "b1"
+                name = "b1"
+                nextState("s2")
             }
         }
 
         state("s2") {
             enter {
-                this.name = "b2"
+                name = "b2"
             }
         }
     }
     agStateMachine.run()
 
     println(agStateMachine.states.toString())
+    println(agStateMachine.runningState.name)
     println(agStateMachine.states.size)
     println(agStateMachine.states[0].block)
 }
